39f0a2468ca5afe4b1a88e4aedaecc455f1290ec
peteflorence <pete.florence@gmail.com>
2016-01-19 16:07:02 -0500

    woot! works without all RL code

--- runSimple.py
+++ runSimple.py
@@ -14,16 +14,7 @@ sim.percentObsDensity      = 4
 sim.nonRandomWorld         = True
 sim.circleRadius           = 2.5
 sim.worldScale             = 1
-sim.supervisedTrainingTime = 3000
-sim.learningTime           = 300
 sim.defaultControllerTime  = 300
 
-
-sim.supervisedTrainingTime = 10
-sim.learningTime = 10
-sim.defaultControllerTime = 1000
-
-sim.options['SARSA']['burnInTime'] = sim.supervisedTrainingTime/2.0
-sim.options['Reward']['actionCost'] = 0.5
 sim.initialize()
 sim.run()
